80f90169fbc030afc474be8eba6f4316c17a3dd3
Jorropo <jorropo.pgm@gmail.com>
2023-07-01 17:56:18 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jorropo/jsync
+
+go 1.20
---
+++ jsync_test.go
@@ -0,0 +1,40 @@
+package jsync_test
+
+import (
+	"testing"
+
+	. "github.com/Jorropo/jsync"
+)
+
+func TestFWaitGroup(t *testing.T) {
+	var called bool
+	f := func() {
+		called = true
+	}
+	var fwg *FWaitGroup
+	shouldNot := func() {
+		t.Helper()
+		fwg.Done()
+		if called {
+			t.Error("called is true even tho it should not")
+		}
+	}
+	should := func() {
+		t.Helper()
+		fwg.Done()
+		if !called {
+			t.Error("called is false even tho it should not")
+		}
+	}
+
+	fwg = NewFWaitGroup(f, 2)
+	shouldNot()
+	should()
+
+	called = false
+	fwg.Init(f, 2)
+	shouldNot()
+	fwg.Add()
+	shouldNot()
+	should()
+}
